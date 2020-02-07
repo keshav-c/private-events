ce18ce4a88eec653a82b04d076ec585dd001a421
keshav-c <keshav1985@gmail.com>
2020-02-07 16:33:51 +0530

    Revert "create Event model, migrate db"
    This reverts commit 601dcc1f93e8eeb7f4cd2ba799b7e882fc9e7aa5. It reverses
    the creation of Event model and migrations, which were incorrect.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_071721) do
-
-  create_table "events", force: :cascade do |t|
-    t.string "creator"
-    t.datetime "time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_02_05_163316) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
