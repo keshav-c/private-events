e8b10e8444c324e2b25d0f2664dc849356b54a3d
keshav-c <keshav1985@gmail.com>
2020-02-07 12:27:02 +0530

    User model with signup and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?  
+
+  def current_user
+    return nil if cookies.signed[:user_id].nil?
+
+    @current_user ||= User.find(cookies.signed[:user_id])
+  end
+
+  def logged_in?
+    !(current_user).nil?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      cookies.signed[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash.now[:danger] = "User already in db!" if User.exists?(name: user_params[:name])
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
