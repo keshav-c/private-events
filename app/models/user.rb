dce3421936a020132536a490e6def0972d1a637f
keshav-c <keshav1985@gmail.com>
2020-02-09 22:01:40 +0530

    Update interface to show participation and attendance

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :events , inverse_of: 'creator', foreign_key: 'creator_id'
   has_many :attendances
-  has_many :attended_events, through: :attendances, source: :event # , inverse_of: 'attendee'
+  has_many :attended_events, through: :attendances, source: :event
   validates :name, uniqueness: true
 end
