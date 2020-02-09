8c56401e079fada33f9f3affe02243f2ac7042c2
keshav-c <keshav1985@gmail.com>
2020-02-09 13:34:50 +0530

    Model, Migration for many to many relationship between User and Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', inverse_of: 'events',  foreign_key: "creator_id"
+  has_many :attendances
+  has_many :attendees, through: :attendances, source: :user # , inverse_of: 'attended_event'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :events , inverse_of: 'creator', foreign_key: 'creator_id'
+  has_many :attendances
+  has_many :attended_events, through: :attendances, source: :event # , inverse_of: 'attendee'
   validates :name, uniqueness: true
 end
